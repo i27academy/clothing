6f61896031e7cd45d90da5c2fe5afdc1c165904e
Siva <siva@i27academy.com>
2026-01-28 20:08:14 +0530

    added api endpoints

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const produtsServiceLocalHost = 'http://localhost:8132/api'
 const usersServiceLocalHost = 'http://localhost:8232/api'
 
-const produtsServiceDevHost = 'http://34.48.255.154:5132/api' 
-const usersServiceDevHost = 'http://34.48.255.154:5232/api'
+const produtsServiceDevHost = 'http://34.122.97.94:31651/api' 
+const usersServiceDevHost = 'http://34.122.97.94:31558/api'
 
 const produtsServiceTestHost = ''
 const usersServiceTestHost = ''
